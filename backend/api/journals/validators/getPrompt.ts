86f35cae2b587bb3e78f48c2524246f06a492ef6
Yash <yashkhot33@gmail.com>
2025-03-22 19:26:24 +0530

    Revert "adding backend files"
    This reverts commit 5e3b496fac69d58bc99e8abe941e4315d34fd3f7.

--- backend/api/journals/validators/getPrompt.ts
+++
@@ -1,5 +0,0 @@
-import {z} from "zod";
-
-export const getPromptSchema = z.object({
-    entryText: z.string().nonempty({ message: "entryText is required and must be a non-empty string." })
-  });
--- backend/api/journals/validators/journalChat.ts
+++
@@ -1,6 +0,0 @@
-import {z} from "zod";
-
-export const journalChatSchema = z.object({
-    entry_text: z.string(),
-    mode: z.enum(["Stoic", "Sensitive", "Creative", "Analytical"]),
-  });
